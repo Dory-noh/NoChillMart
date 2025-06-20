5ce9bdb0e1b3598252dd1c1d4ebc2c275bc78e38
Dori Noh <no6721@naver.com>
2025-06-20 20:05:59 +0900

    250620
    Character Generator 만들기 2

--- Assets/02.Scrips/CustomerGenerator.cs
+++ Assets/02.Scrips/CustomerGenerator.cs
@@ -8,12 +8,21 @@ public class CustomerGenerator : MonoBehaviour
     public Image image;
     public Text infoText;
     public CustomerData[] alienDataArr;
+    int idx = -1;
 
     public void GenerateCustormer()
     {
-        int idx = Random.Range(0, alienDataArr.Length);
-        CustomerData alienData = alienDataArr[idx];
+        int temp = -1;
+        //중복 등장 막음
+        do
+        {
+            temp = Random.Range(0, alienDataArr.Length);
+        }
+        while (temp == idx);
+        idx = temp;
         Debug.Log(idx);
+        CustomerData alienData = alienDataArr[idx];
+        
         image.sprite = alienData.characterImg;
         infoText.text = $"출신 행성 : {alienData.planet}\n" +
             $"종족 이름 : {alienData.raceName}\n" +
